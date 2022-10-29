ae912f2d3bd28cf58abcccaccbec8523cd2d50c9
arazmaharramli <araz.maharramli@novum.az>
2022-10-30 05:35:04 +0800

    Added Sidebar and userpanel, Updated room details page

--- src/Presentation/WebUI/Startup.cs
+++ src/Presentation/WebUI/Startup.cs
@@ -176,13 +176,14 @@ namespace WebUI
                 endpoints.MapControllerRoute(
                     name: "areaswithlang",
                     pattern: "{lang}/{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapControllerRoute(
                     name: "areas",
                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Rooms}/{action=Index}/{id?}");
             });
         }
 
